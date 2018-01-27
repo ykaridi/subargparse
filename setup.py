30caa1312fae931b04cb94140da683d265630fb6
ykaridi <ykaridi@gmail.com>
2018-01-27 22:12:14 +0200

    Added documentation

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def readfile(fpath):
 
 setup(
     name="subargparse",
-    version="1.0",
+    version="1.1",
     author="Yonatan Karidi Heller",
     author_email="ykaridi@gmail.com",
     description="A module for better handling sub-commands in argparse",
     license="MIT",
     keywords="subparser subcommand argparse",
     url="http://github.com/ykaridi/subargparse",
-    download_url='https://github.com/ykaridi/subargparse/archive/1.0.tar.gz',
+    download_url='https://github.com/ykaridi/subargparse/archive/1.1.tar.gz',
     long_description=readfile('README'),
     packages=['subargparse'],
     install_requires=['argparse']
--- subargparse/__init__.py
+++ subargparse/__init__.py
@@ -1,7 +1,17 @@
 def subparser_decorator():
+    '''
+    Creates a custom subparser decorator
+    :return: Decorator
+    '''
     tree = {}
 
     def get_entry(t, *path):
+        '''
+        Gets the entry at the specified path in a tree
+        :param t: Tree
+        :param path: Entry path
+        :return: Entry value
+        '''
         if isinstance(path[0], list) or isinstance(path[0], tuple):
             path = path[0]
         entry = t
@@ -13,6 +23,13 @@ def subparser_decorator():
         return entry
 
     def set_entry(t, v, *path):
+        '''
+        Sets an entry at the specified path in a tree
+        :param t: Tree
+        :param v: Entry value
+        :param path: Entry Path
+        :return: None
+        '''
         if isinstance(path[0], list) or isinstance(path[0], tuple):
             path = path[0]
         entry = t
@@ -26,21 +43,50 @@ def subparser_decorator():
         entry[path[-1]] = v
 
     def wrapper(*path):
+        '''
+        Decorator wrapper function
+        :param path: Subcommand path
+        :return: Inner wrapper
+        '''
         def wrapper_inner(func):
+            '''
+            Inner decorator wrapper function
+            :param func: Subcommand function
+            :return: The original function unchanged
+            '''
             set_entry(tree, func, path + (func.__name__,))
             return func
 
         return wrapper_inner
 
     def bind(self, parser):
+        '''
+        Binds the subparser to an argparse parser
+        :param self: subparser object
+        :param parser: argparse parser
+        :return: None
+        '''
         def configure_help(p, path):
+            '''
+            Configures help for a partial command
+            :param p: Parser
+            :param path: Path
+            :return: None
+            '''
             p.set_defaults(func=lambda x: parser.parse_args(reversed(path + ["-h"])))
 
-        def configure_parser(d, p, path):
-            configure_help(p, path)
-            sps = p.add_subparsers(title='subcommands',
+        def configure_parser(t, parser, path):
+            '''
+            Recursively configures subcommands
+            :param t: Subcommand tree
+            :param p: Parser
+            :param path: Current path
+            :return: None
+            '''
+            configure_help(parser, path)
+            sps = parser.add_subparsers(title='subcommands',
                                         description='valid subcommands')
-            for k, v in d.items():
+            for k, v in t.items():
                 if isinstance(v, dict):
                     lp = sps.add_parser(k)
                     configure_parser(v, lp, path + [k])
@@ -50,7 +96,13 @@ def subparser_decorator():
                     lp.set_defaults(func=action)
 
         def handle(self, args):
-            args.func(args)
+            '''
+            Handles an argument according to the correct subcommand
+            :param self: argparse parser object
+            :param args: Command arguments
+            :return: Function result
+            '''
+            return args.func(args)
         parser.handle = handle.__get__(parser, parser.__class__)
 
         configure_parser(self.tree, parser, [])
